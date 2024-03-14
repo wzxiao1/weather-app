44cb25371840dcc540a784ac48be14b9d48d9cc7
William <williamwillremember1715@gmail.com>
2024-03-14 20:45:36 +1300

    first iteration

--- weatherstuff/weather.py
+++ weatherstuff/weather.py
@@ -1,22 +1,39 @@
 from requests_html import HTMLSession 
-exit = False
+exit_status = False
 s = HTMLSession() # intit HTMLSESSION class
 
 #where we want the weather to be
-while not exit:
-    query = input("Which city's weather would you like to see? ")
-    url = f'https://www.google.com/search?q={query}+weather' # url that
+while not exit_status:
+    try:
+        query = input("Which city's weather would you like to see? ")
+        url = f'https://www.google.com/search?q={query}+weather' 
+        r = s.get(url, headers={'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'})
+        # looking for units
+        unit_status = False
+        while not unit_status:
+            unit_in = input("In which unit do you wish to view the temperature in? (f/c)")
+            if unit_in == "c":        
+                temp = r.html.find('div.Ab33Nc', first = True).find('span#wob_tm', first = True).text
+                unit = r.html.find('div.vk_bk.wob-unit', first = True).text[0:2]
+                unit_status = True
+            elif unit_in == 'f':
+                temp = r.html.find('div.Ab33Nc', first = True).find('span#wob_ttm', first = True).text
+                unit = r.html.find('div.vk_bk.wob-unit', first = True).text[4:6]
+                unit_status = True
+            else:
+                print("Please enter the correct units")
 
-
-
-#gets the url
-    r = s.get(url, headers={'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'})
-
-#finding span#wob_tm which is the temperature
-    temp = r.html.find('div.Ab33Nc', first = True).find('span#wob_tm', first = True).text
-    celsius = r.html.find('div.vk_bk.wob-unit', first = True).text[0:2]
-    description = r.html.find('div.VQF4g', first = True).find('span#wob_dc', first = True).text.lower()
-
-
-    print(f"It looks like in {query} the temperature is around {temp}{celsius} and it is {description}!")
-    exit = input("Type 'True' to exit and press any buttons to continue: ")
+        description = r.html.find('div.VQF4g', first = True).find('span#wob_dc', first = True).text.lower()
+        print(f"It looks like in {query} the temperature is around {temp}{unit} and it is {description}!")
+        if "sunny" in description:
+            print("Be sure to wear a cap and some shades!")
+        elif "shower" in description:
+            print("Be sure to bring an umbrella!")
+        exit = input("Type '0' to exit and press any buttons to continue: ")
+        if exit == '0':
+            exit_status = True
+    except:
+        print("Please enter a correct city!")
+        exit = input("Type '0' to exit and press any buttons to continue: ")
+        if exit == '0':
+            exit_status = True
